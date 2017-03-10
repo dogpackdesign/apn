76dabe3a1343fa56e83591d194054e835858f202
Eric P <proulx712@gmail.com>
2017-03-09 21:14:47 -0800

    updated lookup functions

--- apn/apn.py
+++ apn/apn.py
@@ -1,6 +1,7 @@
 import pickle
 import re
 
+import jellyfish
 from pkg_resources import resource_stream
 
 APNS = []
@@ -24,6 +25,32 @@ def load_apns():
         for s in pickle.load(pklfile):
             apn = Apn(**s)
             APNS.append(apn)
+
+
+def lookup(state=None, county=None):
+
+    if state is None:
+        if ABBR_RE.match(state):
+            state = state.upper()
+            state_field = 'state_abbrev'
+        else:
+            state = jellyfish.metaphone(unicode(state, encoding='utf-8'))
+            state_field = 'state_name_metaphone'
+    elif state is not None:
+        state_field = 'state'
+
+    if county is None:
+        county = jellyfish.metaphone(unicode(county, encoding='utf-8'))
+        county_field = 'county_name_metaphone'
+
+    elif county is not None:
+        county_field = 'county'
+
+    to_return = []
+    for apn in APNS:
+        if state == getattr(apn, state_field) and county == getattr(apn, county_field):
+            to_return.append(apn)
+    return to_return
 
 
 load_apns()
