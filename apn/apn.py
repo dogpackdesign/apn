eb9521f5e6a67baaca2e1f9627f35a24675eb6bc
Eric P <proulx712@gmail.com>
2017-03-09 21:51:38 -0800

    updated lookup

--- apn/apn.py
+++ apn/apn.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import sre_constants
 
 import jellyfish
 from pkg_resources import resource_stream
@@ -28,7 +29,6 @@ def load_apns():
 
 
 def lookup(state=None, county=None):
-
     if state is None:
         if ABBR_RE.match(state):
             state = state.upper()
@@ -51,6 +51,32 @@ def lookup(state=None, county=None):
         if state == getattr(apn, state_field) and county == getattr(apn, county_field):
             to_return.append(apn)
     return to_return
+
+
+def validate(apn_input, state=None, county=None):
+    regex_add = []
+    if state and county is not None:
+        for apn in APNS:
+            if state == getattr(apn, 'state_abbrev') and county == getattr(apn, 'county'):
+                regex_add.append(re.compile(apn.apn_regex))
+
+    elif county is None and state is not None:
+        for apn in APNS:
+            if state == getattr(apn, 'state_abbrev'):
+                regex_add.append(re.compile(apn.apn_regex))
+
+    elif state is None and county is None:
+        for apn in APNS:
+            try:
+                regex_add.append(re.compile(apn.apn_regex))
+            except sre_constants.error:
+                print apn.state, apn.county, apn.apn_regex
+                pass
+
+    if any(regex.match(apn_input) for regex in regex_add):
+        return True
+    else:
+        return False
 
 
 load_apns()
