b53cc66badc96ef983ede1f3aca2362644de0886
Eric P <proulx712@gmail.com>
2017-03-09 19:54:37 -0800

    added db

---
+++ uploader.py
@@ -0,0 +1,18 @@
+import json
+
+import sqlite3
+
+
+def main():
+    file = open('regex.json', 'r')
+    conn = sqlite3.connect('apn.db')
+    sql = '''INSERT INTO APN(apn_format, apn_regex, county, state, state_abbrev) VALUES(?, ?, ?, ?, ?)'''
+    cur = conn.cursor()
+    dict = json.load(file)
+    for state, value in dict.iteritems():
+        for county, other2 in value.iteritems():
+            for regex, format in zip(other2['regex'], other2['formats']):
+                cur.execute(sql, (format, regex, county, None, state))
+    conn.commit()
+if __name__ == '__main__':
+    main()
