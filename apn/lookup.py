5c8bd24bcc6d880e09e84677e335bfa8224778e6
Eric P <proulx712@gmail.com>
2017-03-11 11:32:59 -0800

    rebase stuff

--- apn/lookup.py
+++ apn/lookup.py
@@ -27,3 +27,25 @@ def lookup(state=None, county=None):
         if state == getattr(apn, state_field) and county == getattr(apn, county_field):
             to_return.append(apn)
     return to_return
+
+
+def lookupken(state=None, county=None):
+    results = []
+    for obj in APNS:
+        if state:
+            if obj.state_abbrev == state or obj.state == state:
+                if county:
+                    if obj.county == county:
+                        results.append(obj.apn_format)
+                        print obj.state, obj.county, obj.apn_format
+                    else:
+                        pass
+                else:
+                    results.append(obj.apn_format)
+                    print obj.state, obj.county, obj.apn_format
+            else:
+                pass
+        else:
+            results.append(obj.apn_format)
+            print obj.state, obj.county, obj.apn_format
+    return results
