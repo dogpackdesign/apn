4a265a9dd59ff8ce98c943a505dd719efb31ea6f
Eric P <proulx712@gmail.com>
2017-03-11 13:07:23 -0800

    updated validation

--- apn/validate.py
+++ apn/validate.py
@@ -14,22 +14,43 @@ def validate(apn_input, state=None, county=None):
             state = state.upper()
             state_field = 'state_abbrev'
         else:
-            state = max(APNS, key=lambda x: fuzz.ratio(state, x.state)).state
+            fuzzy_state = max(APNS, key=lambda x: fuzz.ratio(state, x.state)).state
+            if fuzz.ratio(state, fuzzy_state) < 80:
+                raise Exception('Invalid input state: ' + state)
+            state = fuzzy_state
             state_field = 'state'
         state_sub = filter(lambda x: state == getattr(x, state_field), APNS)
+        if not state_sub:
+            raise Exception('Invalid input state: ' + state)
 
     # county stuff
     county_sub = []
     if county is not None and state_sub:
-        county = max(state_sub, key=lambda x: fuzz.ratio(county, x.county)).county
+        fuzzy_county = max(state_sub, key=lambda x: fuzz.ratio(county, x.county)).county
+        if fuzz.ratio(county, fuzzy_county) < 80:
+            raise Exception('Invalid input county: ' + county)
+        county = fuzzy_county
         county_field = 'county'
         county_sub = filter(lambda x: county == getattr(x, county_field), state_sub)
+    elif county is not None and not state_sub:
+        raise Exception('State is required when searching by county')
 
-    apn_results = []
     if county_sub:
-        try:
-            apn_results = filter(lambda x: re.compile(x.apn_regex).match(apn_input), county_sub)
-        except sre_constants as e:
-            print 'ERROR: poorly formatted REGEX'
-            pass
+        apn_results = find_apn(apn_input=apn_input, apn_list=county_sub)
+    elif not county_sub and state_sub:
+        apn_results = find_apn(apn_input=apn_input, apn_list=state_sub)
+    else:
+        apn_results = find_apn(apn_input=apn_input, apn_list=APNS)
+
+    return apn_results
+
+
+def find_apn(apn_input, apn_list):
+    try:
+        apn_results = filter(lambda x: re.compile(x.apn_regex).match(apn_input), apn_list)
+    except sre_constants as e:
+        print 'ERROR: poorly formatted REGEX'
+        pass
+    if not apn_results:
+        raise Exception('No formats for given APN')
     return apn_results
